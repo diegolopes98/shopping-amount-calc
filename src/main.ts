802c45edb84ec76ca1ab55dd91187e5f8ef31874
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-12 15:59:52 -0300

    feat: composing usecases in main file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("teste")
+import { emails, items } from './mocks'
+import { getCustomersAmount } from './data/usecases/get-customers-amount'
+import { getTotalAmount } from './data/usecases/get-total-amount'
+import { getAmountByCostumer } from './data/usecases/get-amount-by-costumer'
+import { fixAmountByCustomer } from './data/usecases/fix-amount-by-customer'
+
+const customersAmount = getCustomersAmount(emails)
+const totalAmount = getTotalAmount(items)
+const amountByCustomer = getAmountByCostumer(totalAmount, customersAmount)
+const amountByCustomerFixed = fixAmountByCustomer(amountByCustomer, customersAmount, totalAmount)
+
+console.log(amountByCustomerFixed)
