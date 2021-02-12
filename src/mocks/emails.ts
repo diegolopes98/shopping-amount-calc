a382932b3d2ca069e9469ebf67a072efdb865c93
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-12 13:38:27 -0300

    feat: add mocks

---
+++ src/mocks/emails.ts
@@ -0,0 +1,5 @@
+export const emails: String[] = [
+  'mail@mail.com',
+  'mail2@mail.com',
+  'mail3@mail.com'
+]
---
+++ src/mocks/items.ts
@@ -0,0 +1,19 @@
+import Item from '../protocols/item'
+
+export const items: Item[] = [
+  {
+    name: 'Produto de teste 1',
+    quantity: 1,
+    amount: 500
+  },
+  {
+    name: 'Produto de teste 2',
+    quantity: 3,
+    amount: 100
+  },
+  {
+    name: 'Produto de teste 3',
+    quantity: 2,
+    amount: 200
+  }
+]
