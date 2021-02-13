fca007647c69cc00481f4f390b43f9264bc098b9
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-13 17:56:18 -0300

    feat: implement check float in presenter

--- src/presentation/presenter/get-data-amount-calculated/get-data-amount-calculated.ts
+++ src/presentation/presenter/get-data-amount-calculated/get-data-amount-calculated.ts
@@ -1,11 +1,12 @@
 import { createPaymentMap, fixAmountByCustomer, fixAmountSumStrategy, getAmountByCostumer, getCustomersAmount, getTotalAmount, mapItemAmount } from './helpers'
-import { Amount } from '../../protocols'
+import { Amount, Item } from '../../protocols'
 import { roundingFloor } from '../../../utils/math/roundings/floor'
 import { sumReducer } from '../../../utils/reducers'
+import { isFloat } from '../../../utils/math/check-types'
 
 export const getDataAmountCalculatedPresenter = (
   getEmailsData: () => string[],
-  getItemsData: () => any[]
+  getItemsData: () => Item[]
 ): Amount => {
   const emails = getEmailsData()
   if (!emails || emails.length === 0) {
@@ -16,6 +17,13 @@ export const getDataAmountCalculatedPresenter = (
   const items = getItemsData()
   if (!items || items.length === 0) {
     console.error('Items list was empty...')
+    return null
+  }
+
+  const hasFloat = items.map((item: Item) => item.amount).findIndex(isFloat) > 0
+
+  if (hasFloat) {
+    console.error('Items list has float numbers in amount...')
     return null
   }
 
