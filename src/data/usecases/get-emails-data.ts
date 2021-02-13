e22eeb260d66594206e2fb1df972db7aacb8efa5
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-13 18:01:26 -0300

    refactor: rename data to resources

--- src/data/usecases/get-emails-data.ts
+++ src/data/usecases/get-emails-data.ts
@@ -3,6 +3,6 @@ export const getEmailsData = (
   pathGeneratorAdapter: (relativePath: string, dirname: string) => string,
   fsReadJSONFileSyncAdapter: <T>(path: string) => T
 ): string[] => {
-  const emailsPath = pathGeneratorAdapter('../../../data/emails.json', __dirname)
+  const emailsPath = pathGeneratorAdapter('../../../resources/emails.json', __dirname)
   return fsReadJSONFileSyncAdapter<string []>(emailsPath)
 }
