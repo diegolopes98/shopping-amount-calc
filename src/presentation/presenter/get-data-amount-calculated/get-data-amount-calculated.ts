086ebaf3133b2105ab12037854ebaa9c321c48c9
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-12 23:50:22 -0300

    feat: check empty files case

--- src/presentation/presenter/get-data-amount-calculated/get-data-amount-calculated.ts
+++ src/presentation/presenter/get-data-amount-calculated/get-data-amount-calculated.ts
@@ -4,11 +4,19 @@ import { roundingFloor } from '../../../utils/math/roundings/floor'
 import { sumReducer } from '../../../utils/reducers'
 
 export const getDataAmountCalculatedPresenter = (
-  getEmailsData,
-  getItemsData
+  getEmailsData: () => string[],
+  getItemsData: () => any[]
 ): Amount => {
   const emails = getEmailsData()
+  if (!emails || emails.length === 0) {
+    console.error('Emails file was empty...')
+    return null
+  }
   const items = getItemsData()
+  if (!items || items.length === 0) {
+    console.error('Items file was empty...')
+    return null
+  }
   const customersAmount = getCustomersAmount(emails)
   const totalAmount = getTotalAmount(items, mapItemAmount, sumReducer)
   const amountByCustomer = getAmountByCostumer(totalAmount, customersAmount, roundingFloor)
