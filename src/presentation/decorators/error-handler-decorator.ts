0bc880b779a0a54b8298bcfdfe8ceaef52c498db
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-13 17:15:03 -0300

    feat: add errorHandlerDecorator

---
+++ src/presentation/decorators/error-handler-decorator.ts
@@ -0,0 +1,8 @@
+export const errorHandler = <T>(functionToCall: (...args: any[]) => T, ...args: any[]): T => {
+  try {
+    return functionToCall(...args)
+  } catch (error) {
+    console.error(`Error while executing ${functionToCall.name}\n\n`, error)
+    return null
+  }
+}
