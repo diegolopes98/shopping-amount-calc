df7d78e31dd7c22d540bdb44182e8d60e9225071
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-14 16:07:05 -0300

    docs: add data layer function's params docs

--- src/data/usecases/get-items-data.ts
+++ src/data/usecases/get-items-data.ts
@@ -1,5 +1,11 @@
 import { Item } from '../protocols'
 
+/**
+ * @param {function} pathGeneratorAdapter           Function to generate the path, receives a string as a
+ * relative path and return an absolute path in the current os.
+ * @param {function} fsReadJSONFileSyncAdapter      Function to import json, receive an absolute path and
+ * returns a json, generic type must by infered when used.
+ */
 export const getItemsData = (
   pathGeneratorAdapter: (relativePath: string, dirname: string) => string,
   fsReadJSONFileSyncAdapter: <T>(path: string) => T
