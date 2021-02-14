aaac66d8b823b050e7b55d2d897d6a2de9368929
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-14 16:21:28 -0300

    docs: add main layer function's params docs

--- src/main/cli/showResults.ts
+++ src/main/cli/showResults.ts
@@ -1,3 +1,7 @@
+/**
+ * @param {function}  resultsFn          Function that returns the results of the operation over the resources
+ * in root resources folder.
+ */
 export const showResults = (resultsFn: () => any): void => {
   console.log('The results are:')
   console.log(resultsFn())
--- src/main/factories/decorators/error-handler-decorator-factory.ts
+++ src/main/factories/decorators/error-handler-decorator-factory.ts
@@ -1,5 +1,8 @@
 import { errorHandlerDecorator } from '../../../presentation/decorators/error-handler-decorator'
 
+/**
+ * @param {function}  fnToCall   Function that it's going to be called by the decorator.
+ */
 export const errorHandlerDecoratorFactory = <T>(fnToCall: (...args) => T): () => T => {
   return () => {
     return errorHandlerDecorator(fnToCall)
