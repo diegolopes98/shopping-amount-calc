eec5c84e3b617f5a01f0d985769bfef68ac55137
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-14 16:16:58 -0300

    docs: add infra layer function's params docs

--- src/infra/file-system-storage/fs/fs-read-file-adapter.ts
+++ src/infra/file-system-storage/fs/fs-read-file-adapter.ts
@@ -1,5 +1,12 @@
 import fs from 'fs'
 
+/**
+ * @param {function} readFileSync   Function to import a file, receives an absolute path and an encoding type,
+ * returns the file content as string
+ * @param {function} jsonParser     Function to parse to Json, receives an string  and returns an generic type
+ * Json.
+ * @param {string}   path           Absolute path of the file you want to import.
+ */
 const fsReadJSONFileSync = (
   readFileSync: (path: string, options: { encoding: BufferEncoding, flag?: string }) => string,
   jsonParser: (text: string) => Object,
@@ -8,6 +15,9 @@ const fsReadJSONFileSync = (
   return jsonParser(readFileSync(path, { encoding: 'utf-8' }))
 }
 
+/**
+ * @param {string}   path           Absolute path of the file you want to import.
+ */
 export const fsReadJSONFileSyncAdapter = <T>(path: string): T => {
   return fsReadJSONFileSync(fs.readFileSync, JSON.parse, path) as T
 }
--- src/infra/file-system-storage/path/path-generator-adapter.ts
+++ src/infra/file-system-storage/path/path-generator-adapter.ts
@@ -1,10 +1,22 @@
 import { join } from 'path'
 
+/**
+ * @param {function}  joinFn          Function to join the path into the format of the current os, receives an
+ * array of strings and return a string.
+ * @param {string}    relativePath    Relative path as string, must use / (forward slash) to concatenate
+ * directories.
+ * @param {string}    dirname         The current dirname given by the os.
+ */
 const pathGenerator = (joinFn: (...paths: string[]) => string, relativePath: string, dirname: string): string => {
   const pathAsArray: string[] = relativePath.split('/')
   return joinFn(dirname, ...pathAsArray)
 }
 
+/**
+ * @param {string}    relativePath    Relative path as string, must use / (forward slash) to concatenate
+ * directories.
+ * @param {string}    dirname         The current dirname given by the os.
+ */
 export const pathGeneratorAdapter = (
   relativePath: string,
   dirname: string
