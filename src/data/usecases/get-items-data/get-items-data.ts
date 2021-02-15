cc71f70e388cbfbb29002b1d9f58f034db8aaa11
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-15 18:18:40 -0300

    fix: update getItemsData usecase import

--- src/data/usecases/get-items-data/get-items-data.ts
+++ src/data/usecases/get-items-data/get-items-data.ts
@@ -10,6 +10,6 @@ export const getItemsData = (
   pathGeneratorAdapter: (relativePath: string, dirname: string) => string,
   fsReadJSONFileSyncAdapter: <T>(path: string) => T
 ): Item[] => {
-  const itemsPath = pathGeneratorAdapter('../../../resources/items.json', __dirname)
+  const itemsPath = pathGeneratorAdapter('../../../../resources/items.json', __dirname)
   return fsReadJSONFileSyncAdapter<Item []>(itemsPath)
 }
